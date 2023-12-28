7b6d01fedc92bad96e303617d1393b80e4606188
Sandesh Shahi <devsanbid@gmail.com>
2023-12-28 12:19:16 +0545

    added

--- .config/nvim/lua/user/heirline/colors.lua
+++ .config/nvim/lua/user/heirline/colors.lua
@@ -2,7 +2,7 @@ return {
   bg = "none",
   section_bg = "none",
   tabline_bg = "none",
-  buffer_active_bg = "#722F37",
-  buffer_active_fg = "black",
+  buffer_active_bg = "none",
+  buffer_active_fg = "white",
   buffer_bg = "none",
 }
