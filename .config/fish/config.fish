9f5a1237fe4f52edb83bc5fb796e4a001a0cff1f
Sandesh Shahi <devsanbid@gmail.com>
2024-02-01 17:12:26 +0545

    change in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,8 @@ set -x FZF_CTRL_T_OPTS "
   --bind 'ctrl-/:change-preview-window(down|hidden|)'
 "
 set GOOGLEAPI "AIzaSyDnOgJjFJL2k-FUbQhkaGIyO-u743tizg4"
+set -x LIBVIRT_DEFAULT_URL 'qemu:///system'
+set GIT_LFS_SKIP_SMUDGE 1
 
 fish_vi_key_bindings
 
@@ -107,36 +109,6 @@ end
 ## Starship prompt
 if status --is-interactive
    source ("/usr/bin/starship" init fish --print-full-init | psub)
-end
-
-
-## Functions
-# Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
-# function __history_previous_command
-#   switch (commandline -t)
-#   case "!"
-#     commandline -t $history[1]; commandline -f repaint
-#   case "*"
-#     commandline -i !
-#   end
-# end
-#
-# function __history_previous_command_arguments
-#   switch (commandline -t)
-#   case "!"
-#     commandline -t ""
-#     commandline -f history-token-search-backward
-#   case "*"
-#     commandline -i '$'
-#   end
-# end
-
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
 end
 
 if not test -x /usr/bin/yay; and test -x /usr/bin/paru
