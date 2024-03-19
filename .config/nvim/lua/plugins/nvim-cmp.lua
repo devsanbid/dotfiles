3148dd1796c2c4d03045a1840617fe1053fc6f6a
devsanbid <devsanbid@gmail.com>
2024-03-19 19:36:20 +0545

    "22861"

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,6 +2,7 @@ return {
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
+      { 'L3MON4D3/LuaSnip' },
       'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets', -- useful snippets
       'SirVer/ultisnips',
