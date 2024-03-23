a253296c3ed88b6bcc318e7569ffa1031b5734fb
devsanbid <devsanbid@gmail.com>
2024-03-23 20:56:54 +0545

    "17036"

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,5 +1,38 @@
+-- Use lsp_zero to manage lsp attachments.
+local lsp_zero = require 'lsp-zero'
+lsp_zero.extend_lspconfig()
 local lspconfig = require 'lspconfig'
 
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      inlayHints = true,
+      diagnostics = {
+        enable = true,
+        globals = {
+          'vim',
+          'describe',
+          'it',
+          'before_each',
+          'after_each',
+          'teardown',
+          'pending',
+        },
+      },
+      runtime = { version = 'LuaJIT' },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          '${3rd}/luv/library',
+          unpack(vim.api.nvim_get_runtime_file('', true)),
+        },
+      },
+      completion = {
+        callSnippet = 'Replace',
+      },
+    },
+  },
+}
 vim.diagnostic.config {
   underline = true,
   virtual_text = {
@@ -44,7 +77,24 @@ lspconfig.rust_analyzer.setup {
   },
 }
 
+lsp_zero.on_attach(function(_, bufnr)
+  lsp_zero.default_keymaps { buffer = bufnr }
+end)
 
+-- Setup Mason and Mason-Config.
+require('mason').setup {}
+require('mason-lspconfig').setup {
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+  },
+}
+
+--cmp
+-- import nvim-cmp plugin safely
 local cmp_status, cmp = pcall(require, 'cmp')
 if not cmp_status then
   return
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -25,6 +25,7 @@ return {
           'yamlls',
           'pyright',
           'tailwindcss',
+          'tsserver',
           'clangd',
           'taplo',
           'biome',
@@ -33,69 +34,21 @@ return {
     end,
   },
 
+  { -- LSP Support
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    lazy = true,
+    config = false,
+    keys = {
+      { '<leader>i', '<cmd>LspInfo<cr>',    'Lsp Info' },
+      { '<leader>I', '<cmd>LspInstall<cr>', 'Lsp Install' },
+    },
+  },
+
   { -- LSP Config
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      { 'j-hui/fidget.nvim',   opts = {} },
     },
-
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-          end
-
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          map('<leader>lr', vim.lsp.buf.rename, '[R]e[n]ame')
-
-          map('<leader>la', vim.lsp.buf.code_action, '[C]ode [A]ction')
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        end,
-      })
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-      }
-
-      require('mason').setup()
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-    end
   },
 }
